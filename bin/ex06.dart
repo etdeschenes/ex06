4e5635d35b4bd85bfe25b57afd25a3551cab6668
etdeschenes <etienne.deschenes.1@ulaval.ca>
2014-10-07 01:13:12 -0400

    update ex06

--- bin/ex06.dart
+++ bin/ex06.dart
@@ -1,7 +1,7 @@
 import 'package:ex06/fonctions.dart';
 
 //No. 1
-no1() {
+void no1() {
   var words = ['radar', 'soleil', 'kayak'];
   var test;
   print('#1 : Est-ce que les mots suivants sont des palindrômes ?');
@@ -13,16 +13,16 @@ no1() {
 }
 
 //No. 2
-no2() {
+void no2() {
   var date1 = new DateTime.now();
   var date2 = new DateTime(2014, 9, 2);
-  print('#2 : La différence de jours entre 2 dates.');
-  differenceDate(date1, date2);
+  print('#2 : La différence de jours entre $date1 et $date2.');
+  print('${differenceDate(date1, date2)} jours');
   print("");
 }
 
 //No. 3
-no3() {
+void no3() {
   List gradeList = [59,76,85,98];
   var answer;
   print('#3 : La conversion de notes numériques en lettres.');
@@ -34,21 +34,26 @@ no3() {
 }
 
 //No. 4
-no4() {
+void no4() {
   print('#4 : Listes de noms avec moins, plus ou 8 lettres.');
   List start = ['Flash', 'Sargent', 'Guillermo', 'Buster', 'Jinxopah', 'Rafaella', 'Estelle', 'Thalassa', 'Delphinia', 'Ellettra'];
   print('Liste principale de noms : $start');
-  namesList(start);
+  List part = namesList(start);
+  print('Liste de noms de moins de 8 lettres : ${part.elementAt(0)}');
+  print('Liste de noms de 8 lettres : ${part.elementAt(1)}');
+  print('Liste de noms de plus de 8 lettres : ${part.elementAt(2)}');
   print("");
 }
 
 //No. 5
-no5() {
+void no5() {
   List playersName = [['Malkin', 'Crosby'], ['Souray', 'Perry'], ['Karlsson', 'Phillips'], ['Johnson', 'Dubinsky'], ['Luongo', 'Hayes'], ['Tavares', 'Vanek']];
   List clubsName = ['Penguins', 'Ducks', 'Senators', 'Blue Jackets', 'Panthers', 'Islanders'];
   print('#5 : Liste des clubs avec les joueurs associés.');
-  playersClubs(playersName, clubsName);
-  print('');
+  print('Liste des joueurs : $playersName');
+  print('Liste des clubs : $clubsName');
+  print('Liste des clubs associés aux joueurs : ${playersClubs(playersName, clubsName)}');
+  print("");
 }
 
 void main() {
--- lib/fonctions.dart
+++ lib/fonctions.dart
@@ -2,24 +2,23 @@ library fonctions;
 
 //No. 1
 //Source : http://fr.openclassrooms.com/forum/sujet/comparer-si-une-string-est-un-palindrome-18371
-palindrome(var word) {
+bool palindrome(var word) {
   for (int i = 0; i < word.length ~/ 2; i++) {
-    if (word[i] != word[word.length - (1 + i)]) { return false;
-    } else {
-    return true;
-    }
+    if (word[i] != word[word.length - (1 + i)]) return false;
   }
+  return true;
 }
 
 //No. 2
-differenceDate(var date1, var date2) {
-  var difference = date1.difference(date2);
-  print('${difference.inDays} jours');
+int differenceDate(var date1, var date2) {
+  var difference;
+  difference = date1.difference(date2);
+  return difference.inDays;
 }
 
 //No. 3
 //Source : https://docs.google.com/presentation/d/1E8IRYDNk9l9UIvdoPlKp2iaraEvejxzHkgYpON6nCMo/edit?pli=1#slide=id.g2e2d4b4f_0_27
-gradeClassification(var grade) {
+String gradeClassification(var grade) {
   var letter = "";
   if (grade >= 95) {
     return letter += "A+";
@@ -43,17 +42,18 @@ gradeClassification(var grade) {
      return letter += "D+";
    } else if (grade <= 63.5 && grade > 59.99) {
      return letter += "D";
-   } else if (grade <= 60 && grade > 0) {
+   } else {
      return letter += "E";
   }
 }
 
 //No. 4
 //Source : https://api.dartlang.org/apidocs/channels/stable/dartdoc-viewer/dart:core.List
-namesList(List start) {
-  var namesLess8 = new List();
-  var namesEqual8 = new List();
-  var namesMore8 = new List();
+List namesList(List start) {
+  List namesLess8 = new List();
+  List namesEqual8 = new List();
+  List namesMore8 = new List();
+  List allNames = new List();
   for (var name in start) {
     if (name.length < 8) {
       namesLess8.add(name);
@@ -61,19 +61,18 @@ namesList(List start) {
       namesEqual8.add(name);
     } else namesMore8.add(name);
   }
-  print('Liste de noms de moins de 8 lettres : $namesLess8');
-  print('Liste de noms de 8 lettres : $namesEqual8');
-  print('Liste de noms de plus de 8 lettres: $namesMore8');
+  allNames.add(namesLess8);
+  allNames.add(namesEqual8);
+  allNames.add(namesMore8);
+  return allNames;
 }
 
 //no. 5
 //Source : http://stackoverflow.com/questions/18244545/dart-how-to-sort-maps-keys
-playersClubs(var playersName, var clubsName) {
+Map playersClubs(List playersName, List clubsName) {
   var map = new Map();
   for (var i = 0; i < 6; i++) {
     map [clubsName[i]] = playersName[i];
   }
-  for (var y in map.keys) {
-    print('$y : ${map[y]}');
-  }
+  return map;
 }
